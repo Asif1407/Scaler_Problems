4adc17e14c97c3eb5885159b6c0d876b0623f24d
Asif1407 <47074816+Asif1407@users.noreply.github.com>
2022-08-16 01:05:01 +0530

    Added new problems

--- Recursion.py
+++ Recursion.py
@@ -15,7 +15,33 @@ def fibonacci(n):
         return 1
     return fibonacci(n-1) + fibonacci(n-1)
 
+def printInc(n):
+    if n==0 :
+        return
+    printInc(n-1)
+    print(n, end=",")
 
-print(fibonacci(5))
-print(recursivefactorial(4))
-print(recursivesum(100))
+
+def printdec( n):
+    if n ==1:
+        print(1)
+        return
+    print(n, end =',')
+    printdec(n-1)
+
+def recursivepalindrome( string, s, e):
+    if s>=e:
+        return True
+    if (string[s]==string[e]):
+        if (recursivepalindrome(string, s+1, e-1)):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+variable = "globle"
+print(recursivepalindrome(variable,0,len(variable)-1))
+
+
+printdec(7)
